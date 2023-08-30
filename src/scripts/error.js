28f4f042eed0f1ccb6090cf30245dabb2adc0e92
Gabriel <gabriel_camillosouza@hotmail.com>
2023-08-30 20:01:16 -0300

    Entrega 4 | Git Search

--- src/scripts/error.js
+++ src/scripts/error.js
@@ -2,7 +2,7 @@ const redirectToHomePage = () => {
     const redirectButton = document.querySelector(".new-search__button");
 
     redirectButton.addEventListener("click", () => {
-        location.replace("http://127.0.0.1:5500");
+        location.replace("../../");
     });
 }
 redirectToHomePage();
